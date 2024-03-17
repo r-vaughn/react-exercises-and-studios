068786558a77e312051a3a43b457880de923dc3e
r-vaughn <rebecca.a.vaughn@outlook.com>
2024-03-16 22:02:06 -0500

    Ingredients list publishing now

--- part2/studio/src/components/Ingredients.jsx
+++ part2/studio/src/components/Ingredients.jsx
@@ -2,22 +2,38 @@ import recipedata from "./recipe.json";
 
 function IngredientList() {
   
-  const Ingredient = recipedata.map((item) => (
-    <li key={item.ingredients}>
-      {item.ingredients}
-    </li>
-  ))
+  // const Ingredient = recipedata.map((ingredients) => (
+  //   <li>
+  //     {Ingredient.index}
+  //   </li>
+  // ))
+  // return (
+  //   <div>
+  //     <h3>Ingredients</h3>
+  //     <ul>
+  //       {Ingredient}       
+  //     </ul>
+  //  </div>
+  // );
   return (
     <div>
-      <h3>Ingredients</h3>
-      <ul>
-        {ingredient}       
-      </ul>
-   </div>
-  );
- }
- 
- export default IngredientList;
+    <h3>Ingredients</h3>
+      {recipedata.map((e) => {
+        return (
+          <ul>
+            {e.ingredients.map((ingredient, index) => {
+              return (
+                <li key={index}>{ingredient}</li>
+              )
+            })}
+          </ul>
+        )
+     })}
+    </div>
+  )
+}
+
+export default IngredientList;
  
 //import json file for the data
 //use a nested map to get inside the inner array
