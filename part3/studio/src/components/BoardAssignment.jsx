f6c597c336c45206c30df7f2301641d8f7930b6f
r-vaughn <rebecca.a.vaughn@outlook.com>
2024-04-01 15:37:58 -0500

    Studio update

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,9 +1,39 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
+   const boards = [ 
+      {
+         "label": "Dinner Recipes",
+         "value": 0      
+      }, 
+      {
+         "label": "Slow Cook Meals",
+         "value": 1
+      }, 
+      {
+         "label": "Gabe Kennedy Recipes",
+         "value": 2
+      }
+   ];
+
+   let boardOptions = boards.map() =>
+      return ( 
+         <option key={index} value={boards.value}>{boards.label}</option> 
+         <select>
+            {boards.map((each) => {
+             return (
+                {each.boards.map((boards, index) => {
+                   return (
+                      
+                   )
+                 })}
+               )
+            })}
+         </select>
+       )
 
    const handleChange = (event) => {
+      setBoardName(event.target.value);
    }
 
    return (
--- part3/studio/src/components/Recipe.jsx
+++ part3/studio/src/components/Recipe.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://www.instagram.com/gabe_kennedy/?hl=en";
+   let authorPhoto = "https://images.squarespace-cdn.com/content/v1/6189edeba65e873a7aa4b5fd/9fb42a1c-be9b-452c-b7af-fa9a46db8f84/7291D58E-C9C8-4E95-9B32-4004F129BB61+2.jpg?format=1500w";
+   let authorName = "Gabe Kennedy";
 
    return (
       <div>
@@ -17,7 +17,7 @@ const RecipeAuthor = () => {
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ["Short Ribs", "Onions", "Carrots", "Fresh Herbs", "Red Wine"];
    return(
       <div>
          <h3>Recipe Ingredients</h3>
@@ -36,8 +36,8 @@ const RecipeDescription = () => {
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Braised Short Ribs</h1>
+            <p>Classic braised short ribs in a red wine sauce. The perfect comfort food.</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -49,7 +49,7 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://th.bing.com/th/id/OIP.CqWaJwGVM9PIkyaKesb80wHaHa?w=181&h=181&c=7&r=0&o=5&pid=1.7" alt="Plated Red Wine Braised Short Ribs" className=""/>
    );
 }
 
